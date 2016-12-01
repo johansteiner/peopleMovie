318b1745097d1bb4de6112e9e88996cf5e3bffec
johansteiner <johan.steiner@he-arc.ch>
2016-12-01 08:31:15 +0100

    creation classes

---
+++ src/main/java/ch/hearc/ig/odi/peoplemovie/business/Person.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplemovie.business;
+
+/**
+ *
+ * @author johan.steiner
+ */
+public class Person {
+    
+    private Long id;
+    private String firstname;
+    private String lastname;
+
+    public Person(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+    
+}
